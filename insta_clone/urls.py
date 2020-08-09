76fe82fe39cf5667140bd69377df3c13c53188e1
antony-guthiga <toniguthiga93@gmail.com>
2020-08-10 01:17:29 +0300

    add url paths for register,profile,login,logout and app url

--- insta_clone/urls.py
+++ insta_clone/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url,include
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import views as auth_views
+from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    url(r'^register/', user_views.register, name='register'),
+    url(r'^profile/', user_views.profile, name='profile'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    url(r'', include('insta.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
