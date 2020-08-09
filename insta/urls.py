93ebb4d23016bb4a3cc96d36e68cd303351737c1
antony-guthiga <toniguthiga93@gmail.com>
2020-08-10 01:11:40 +0300

    add home url for app

---
+++ insta/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url('^$',views.index, name='index'),
+]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
