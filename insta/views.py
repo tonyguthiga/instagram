78cb668ca8a731048f41e445ace5d6f7fb2d6b90
antony-guthiga <toniguthiga93@gmail.com>
2020-08-10 01:12:11 +0300

    add homepage view function

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts':posts})
