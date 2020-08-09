98922a133f8750a4219347345faf215d987fb2c2
antony-guthiga <toniguthiga93@gmail.com>
2020-08-10 01:10:52 +0300

    update image field

--- insta/models.py
+++ insta/models.py
@@ -4,6 +4,6 @@ from django.utils import timezone
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    image = models.ImageField(blank=True, null=True)
+    image = models.ImageField(upload_to = 'posts/', blank=True)
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
