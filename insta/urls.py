d2e387068a6abe47fa7253f631759e46e9ed44e8
antony-guthiga <toniguthiga93@gmail.com>
2020-08-11 08:46:47 +0300

    add urls for post detail,create,update and delete. add urls for   like,comment and search

--- insta/urls.py
+++ insta/urls.py
@@ -1,10 +1,18 @@
 from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$',views.index, name='index'),
+    url('^$', PostListView.as_view(), name='index'),
+    url('post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
+    url('post/new/', PostCreateView.as_view(), name='post-create'),
+    url('post/(?P<pk>\d+)/update/$', PostUpdateView.as_view(), name='post-update'),
+    url('post/(?P<pk>\d+)/delete/$', PostDeleteView.as_view(), name='post-delete'),
+    url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
+    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
+    url(r'^search/',views.search, name='search'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
