4510b8511220e23cc69b335c49b506c9d1f81702
antony-guthiga <toniguthiga93@gmail.com>
2020-08-11 08:45:04 +0300

    add likes and comment models

--- insta/models.py
+++ insta/models.py
@@ -1,9 +1,51 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
-    image = models.ImageField(upload_to = 'posts/', blank=True)
+    image = models.ImageField()
     caption = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return str(self.name)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def get_profile_images(cls, profile):
+        images = Image.objects.filter(profile__pk=profile)
+        return images
+
+class Likes(models.Model):
+    liker = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    image = models.ForeignKey(Post, blank=True, on_delete=models.CASCADE,related_name='comment')
+    comment_owner = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    comment= models.TextField()
+
+    def save_comment(self):
+        self.save()
+
+    def delete_comment(self):
+        self.delete()
+
+    @classmethod
+    def get_image_comments(cls, id):
+        comments = Comment.objects.filter(image__pk=id)
+        return comments
+
+    def __str__(self):
+        return str(self.comment)
