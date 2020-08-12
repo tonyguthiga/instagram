c034fb8f2ef94c9c28fadf22cfd1ae5135cbc165
antony-guthiga <toniguthiga93@gmail.com>
2020-08-12 12:52:13 +0300

    add user profile

--- users/models.py
+++ users/models.py
@@ -1,18 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
-    bio = models.TextField()
+    bio = models.TextField(max_length=250)
 
+    @receiver(post_save, sender=User)
+    def create_profile_for_new_user(sender, created, instance, **kwargs):
+        if created:
+            profile = Profile(user=instance)
+            profile.save()
+    
     def __str__(self):
         return f'{self.user.username} Profile'
-
-    def save(self):
-        super().save()
 
         img = Image.open(self.image.path)
 
