7f9378c508484456bdd46bd21e9a8c92774e35b1
antony-guthiga <toniguthiga93@gmail.com>
2020-08-12 12:43:29 +0300

    change url to path

--- insta/urls.py
+++ insta/urls.py
@@ -1,18 +1,18 @@
 from . import views
 from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
-from django.conf.urls import url
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$', PostListView.as_view(), name='index'),
-    url('post/(?P<pk>\d+)/$', PostDetailView.as_view(), name='post-detail'),
-    url('post/new/', PostCreateView.as_view(), name='post-create'),
-    url('post/(?P<pk>\d+)/update/$', PostUpdateView.as_view(), name='post-update'),
-    url('post/(?P<pk>\d+)/delete/$', PostDeleteView.as_view(), name='post-delete'),
-    url(r'^like/(?P<image_id>\d+)', views.like, name='like'),
-    url(r'^comment/(?P<image_id>\d+)', views.comment, name='comment'),
-    url(r'^search/',views.search, name='search'),
+    path('', PostListView.as_view(), name='index'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+    path('like/<image_id>/', views.like, name='like'),
+    path('comment/<image_id>/', views.comment, name='comment'),
+    path('search/',views.search, name='search'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
