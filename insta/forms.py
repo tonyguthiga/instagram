b603f48859ea87d3f27fb4560207b5b08be1dea3
antony-guthiga <toniguthiga93@gmail.com>
2020-08-12 12:42:43 +0300

    add postform

--- insta/forms.py
+++ insta/forms.py
@@ -1,9 +1,14 @@
  
 from django import forms
-from .models import Comment
+from .models import Comment, Post
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         exclude = ['image', 'comment_owner']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model  = Post
+        fields = ['image', 'caption']
